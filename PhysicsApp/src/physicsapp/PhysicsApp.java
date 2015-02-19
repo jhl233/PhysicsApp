aeb71cff0a0bedbdc1816b0db239547543a66f28
Matthew Yacavone <myacavone2015@2015-matthew-yacavone.mka.org>
2015-02-18 19:35:12 -0500

    Added the MainFrame class and the CustomJButton class.

--- PhysicsApp/src/physicsapp/PhysicsApp.java
+++ PhysicsApp/src/physicsapp/PhysicsApp.java
@@ -5,6 +5,8 @@
  */
 package physicsapp;
 
+import frontend.MainFrame;
+
 /**
  *
  * @author jlee
@@ -16,6 +18,7 @@ public class PhysicsApp {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+      new MainFrame().setVisible(true);
     }
     
 }
